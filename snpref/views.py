fd972b7ae259777fb8568f468adce1e191e06ec5
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-05-10 17:24:30 +0200

    merge

--- snpref/views.py
+++ snpref/views.py
@@ -2,14 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-<<<<<<< HEAD
-from .models import Phenotype
-from .models import SNP
-=======
->>>>>>> df6522d86bbdd6ef5fd7cfd03e01051d856ed95a
+
 
 # Create your views here.
 from .models import Phenotype
+from .models import SNP
 
 def phenotype_list(request):
     phenotypes = Phenotype.objects.all()
