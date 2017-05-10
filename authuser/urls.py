8e43cc1b7e9dc29085fb7c2779300338f443f509
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-05-10 17:36:47 +0200

    Added slash regexp to authuser

--- authuser/urls.py
+++ authuser/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.connexion, name='connexion'),
-    url(r'^deconnexion$', views.deconnexion, name='deconnexion'),
+    url(r'^deconnexion/?$', views.deconnexion, name='deconnexion'),
 ]
