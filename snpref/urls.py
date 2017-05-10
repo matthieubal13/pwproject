6fdc40f7fa9634ff64ae974415cc315b86e8cdf9
Matthieu <matthieubal@msn.com>
2017-05-10 17:19:31 +0200

    Phenotype list

--- snpref/urls.py
+++ snpref/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.phenotype_list, name='phenotype_list'),
+    url(r'^snp/?$', views.snp_list, name='snp_list'),
 ]
--- snpref/views.py
+++ snpref/views.py
@@ -3,9 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import Phenotype
+from .models import SNP
 
 # Create your views here.
 def phenotype_list(request):
     phenotypes = Phenotype.objects.all()
     return render(request, 'snpref/phenotype_list.html',
     {'phenotypes': phenotypes})
+
+def snp_list(request):
+    snp = SNP.objects.all()
+    return render(request, 'snpref/snp_list.html',
+    {'snp': snp})
