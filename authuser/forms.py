50248aeac2f396a2122ef40dff849a82c2378ba4
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-05-11 22:02:46 +0200

    New home page

--- authuser/forms.py
+++ authuser/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class ConnexionForm(forms.Form):
-    username = forms.CharField(label="User name", max_length=30)
-    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    username = forms.CharField(label="User name", max_length=30,
+    widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    password = forms.CharField(label="Password",
+    widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
