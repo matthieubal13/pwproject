5a2061fdd032a8ab360ddaf076359a96e6245fe6
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-05-17 22:19:49 +0200

    Last update

--- snpref/models.py
+++ snpref/models.py
@@ -5,29 +5,35 @@ from django.db import models
 
 # Create your models here.
 
-# Return the name of the phenotype
 class Phenotype(models.Model):
+    """Represents phenotypes with a trait."""
     trait = models.CharField(max_length = 100)
+    
     def __str__(self):
         return str(self.trait)
 
 
-# Return the name of the SNP
 class SNP(models.Model):
+    """Represents SNP with an ID."""
     rs_id = models.CharField(max_length = 20)
+
     def __str__(self):
         return str(self.rs_id)
 
-# Return the references of a SNP
+
 class Reference(models.Model):
+    """Represents a reference which is simply a URL"""
     url = models.URLField()
+
     def __str__(self):
         return str(self.url)
 
-# Return primary keys of other tables
+
 class SNPRefPhen(models.Model):
+    """Represents the association between a phenotype, a SNP and a reference."""
     phenotype = models.ForeignKey(Phenotype, on_delete = models.CASCADE)
     snp = models.ForeignKey(SNP, on_delete = models.CASCADE)
     ref = models.ForeignKey(Reference, on_delete = models.CASCADE)
+
     def __str__(self):
         return str(self.phenotype) + " - " + str(self.snp)
