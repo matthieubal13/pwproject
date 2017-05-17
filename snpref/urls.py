3fcbed1ea993ca508c8fe3a972ea434e99bdbb90
Matthieu <matthieubal@msn.com>
2017-05-17 19:59:25 +0200

    commentaires des vues, urls et modèles de snpref

--- snpref/urls.py
+++ snpref/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+# Description of the differentes URLs to do the connection with the views
 urlpatterns = [
     url(r'^$', views.phenotype_list, name='phenotype_list'),
     url(r'^snp/?$', views.snp_list, name='snp_list'),
